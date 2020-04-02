2f3e3a9acea5be5a0e91aa4ad76e7e5165ef5af7
ErezHa <49830615+ErezHa@users.noreply.github.com>
2020-04-02 12:12:25 +0300

    The average_mode.mode() always returns the first item, so I fix it.
    The average_mode.mode() always returns the first item, so I fix it.

--- maths/average_mode.py
+++ maths/average_mode.py
@@ -7,25 +7,23 @@ def mode(input_list):  # Defining function "mode."
 
     The input list may contain any Datastructure or any Datatype.
 
-    >>> input_list = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
+    >>> input_list = [3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
     >>> mode(input_list)
     2
-    >>> input_list = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
+    >>> input_list = [3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
     >>> mode(input_list) == statistics.mode(input_list)
     True
     """
-    # Copying input_list to check with the index number later.
-    check_list = input_list.copy()
     result = list()  # Empty list to store the counts of elements in input_list
     for x in input_list:
         result.append(input_list.count(x))
         input_list.remove(x)
-        y = max(result)  # Gets the maximum value in the result list.
-        # Returns the value with the maximum number of repetitions.
-        return check_list[result.index(y)]
+    y = max(result)  # Gets the maximum value in the result list.
+    # Returns the value with the maximum number of repetitions.
+    return input_list[result.index(y)]
 
 
 if __name__ == "__main__":
-    data = [2, 3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
+    data = [3, 4, 5, 3, 4, 2, 5, 2, 2, 4, 2, 2, 2]
     print(mode(data))
     print(statistics.mode(data))
